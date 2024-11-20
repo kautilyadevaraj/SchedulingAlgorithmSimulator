757bb3b2b05463f2d88d40b4adf004b6c3d551cc
Kautilya DK <kautilyadk@gmail.com>
2024-11-20 15:42:23 +0530

    fixed gantt chart heading alignment

--- components/GanttChart.tsx
+++ components/GanttChart.tsx
@@ -56,7 +56,7 @@ const GanttChart: React.FC<GanttChartProps> = ({ processes }) => {
 
   return (
     <div className="w-full shadow-md pt-10">
-      <h2 className="text-lg font-semibold mb-4">Gantt Chart</h2>
+      <h2 className="text-lg font-semibold pl-2 mb-4">Gantt Chart</h2>
       <div className="h-fit p-1">
         {/* Use animationKey to force reanimation */}
         <motion.div
